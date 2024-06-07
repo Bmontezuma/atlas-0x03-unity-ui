71020733c333529cee525d926a2bea51b316613b
Brandon Montezuma <143756794+Bmontezuma@users.noreply.github.com>
2024-06-07 04:32:04 -0500

    Initial commit for project

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject coinPrefab; // Assign the coin prefab in the Inspector
+
+    private Vector3[] coinPositions = new Vector3[]
+    {
+        new Vector3(10, 0.5f, 10),
+        new Vector3(20, 0.5f, 15),
+        new Vector3(5, 0.5f, 30),
+        new Vector3(15, 0.5f, 45),
+        new Vector3(25, 0.5f, 25),
+        new Vector3(35, 0.5f, 10),
+        new Vector3(45, 0.5f, 35),
+        new Vector3(50, 0.5f, 20),
+        new Vector3(40, 0.5f, 40),
+        new Vector3(30, 0.5f, 50),
+        new Vector3(55, 0.5f, 55),
+        new Vector3(60, 0.5f, 10),
+        new Vector3(10, 0.5f, 55),
+        new Vector3(20, 0.5f, 50),
+        new Vector3(25, 0.5f, 5),
+        new Vector3(30, 0.5f, 35),
+        new Vector3(35, 0.5f, 25),
+        new Vector3(50, 0.5f, 45),
+        new Vector3(45, 0.5f, 15),
+        new Vector3(55, 0.5f, 30)
+    };
+
+    void Start()
+    {
+        foreach (Vector3 position in coinPositions)
+        {
+            Instantiate(coinPrefab, position, Quaternion.identity);
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  // Include the SceneManager
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;  // Speed of the player
+    private int score = 0;      // Score of the player
+    public int health = 5;      // Health of the player, initial value 5
+
+    void Start()
+    {
+        // Any initialization if needed
+    }
+
+    void FixedUpdate()
+    {
+        // Get input from the horizontal and vertical axes (arrow keys or WASD)
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Create a Vector3 movement based on input
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Apply the movement to the player
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    void Update()
+    {
+        // Check if health is zero
+        if (health == 0)
+        {
+            // Log "Game Over!" message
+            Debug.Log("Game Over!");
+
+            // Reload the scene to start again
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            // Reset health and score
+            health = 5;
+            score = 0;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the object we collided with is tagged as "Pickup"
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            // Increment the score
+            score++;
+
+            // Log the new score
+            Debug.Log("Score: " + score);
+
+            // Disable the Coin object
+            other.gameObject.SetActive(false);
+        }
+        // Check if the object we collided with is tagged as "Trap"
+        else if (other.gameObject.CompareTag("Trap"))
+        {
+            // Decrement the health
+            health--;
+
+            // Log the new health
+            Debug.Log("Health: " + health);
+
+            // You can add game over logic or other consequences here
+        }
+        // Check if the object we collided with is tagged as "Goal"
+        else if (other.gameObject.CompareTag("Goal"))
+        {
+            // Log "You win!" message
+            Debug.Log("You win!");
+
+            // You can add additional logic for winning the game here
+        }
+    }
+}
